17fcff3d69d9d7cabc87f3d0e4700ff57e466c54
Pavi-29 <110191310+Pavi-29@users.noreply.github.com>
2023-01-19 12:54:49 +0530

    loader

---
+++ Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BasicExample() {
+  return (
+    <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+  );
+}
+
+export default BasicExample;
